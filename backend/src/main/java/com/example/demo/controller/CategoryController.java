4aad802c98675a639735470bc9cde1bb8fda1e8b
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2020-11-17 19:18:46 +0100

    kreiran controller paket i prazne controller klase

---
+++ backend/src/main/java/com/example/demo/controller/CategoryController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/categories", produces = MediaType.APPLICATION_JSON_VALUE)
+public class CategoryController {
+
+	
+	
+}
