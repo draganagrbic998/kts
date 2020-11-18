137ba6c08134ace4dbfd02f4f0de94d4794215ce
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2020-11-18 21:32:23 +0100

    napravljena forma za filter kulturnih ponuda

--- frontend/src/app/cultural-offers/services/cultural.service.ts
+++ frontend/src/app/cultural-offers/services/cultural.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { API_FILTER_NAMES } from '../utils/api';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +13,22 @@ export class CulturalService {
     private http: HttpClient
   ) { }
 
-  
+  filterNames(filterParam: string): Observable<string[]>{
+    return this.http.post<string[]>(API_FILTER_NAMES, filterParam).pipe(
+      catchError(() => of([]))
+    );
+  }
+
+  filterLocations(filterParam: string): Observable<string[]>{
+    return this.http.post<string[]>(API_FILTER_LOCATIONS, filterParam).pipe(
+      catchError(() => of([]))
+    );
+  }
+
+  filterTypes(filterParam: string): Observable<string[]>{
+    return this.http.post<string[]>(API_FILTER_TYPES, filterParam).pipe(
+      catchError(() => of([]))
+    );
+  }
 
 }
