26c753874db433eb76d96aae169c9694639c62a8
Makim1998 <mmijatovic32@gmail.com>
2020-11-25 12:54:59 +0100

    brisanje tipova

--- frontend/src/app/types/type-details/type-details.component.ts
+++ frontend/src/app/types/type-details/type-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ConfirmationDialogComponent } from 'src/app/layout/confirmation-dialog/confirmation-dialog.component';
+import { ERROR_SNACKBAR_OPTIONS, SUCCESS_SNACKBAR_OPTIONS } from 'src/app/utils/constants';
 import { TypeService } from '../services/type.service';
 import { Type } from '../utils/Type';
 
@@ -14,7 +16,7 @@ export class TypeDetailsComponent implements OnInit {
   constructor(private typeService: TypeService,
     private snackBar: MatSnackBar,
     public dialog: MatDialog) { }
-    
+
   @Input() currentType: Type;
   @Output() onRefreshData: EventEmitter<string> = new EventEmitter();
   delPending: boolean = false;
@@ -22,5 +24,31 @@ export class TypeDetailsComponent implements OnInit {
   ngOnInit(): void {
   }
   
+  delete(): void {
+    const dialog: MatDialogRef<ConfirmationDialogComponent> = this.dialog.open(ConfirmationDialogComponent, 
+      {disableClose: true, panelClass: "no-padding", data: "Delete type?"});
+
+      dialog.afterClosed().subscribe((result) => {
+        if (result) {
+          this.delPending = true;
+    
+          this.typeService.delete(this.currentType.id).subscribe(
+            () => {
+              this.delPending = false;
+
+              this.onRefreshData.emit("obrisan tip");
+              console.log("obrisana tip");
+        
+              this.snackBar.open("You have successfully deleted this type!", "Close", SUCCESS_SNACKBAR_OPTIONS);
+            }, 
+            () => {
+              this.delPending = false;
+              this.snackBar.open("Can't delete this type because there are cultural offers that have this type!", 
+              "Close", ERROR_SNACKBAR_OPTIONS);
+            }
+          )
+        }
+      });
+    }
 
 }
