91ec89c16efd890e7a3ee99fbded3b2ef771aef4
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2020-12-24 23:58:53 +0100

    uradila e2e testiranje za izmenu kulturne ponude

---
+++ e2e-testing/src/main/java/com/example/demo/CulturalOfferDialog.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CulturalOfferDialog {
+
+	private WebDriver browser;
+		
+	@FindBy(xpath = "//*/app-cultural-dialog/mat-drawer-container/mat-drawer-content/div/app-spacer-container/div/span[1]/button[1]")
+	private WebElement editButton;
+	
+	@FindBy(xpath = "//*/app-cultural-dialog/mat-drawer-container/mat-drawer-content/div/app-spacer-container/div/span[1]/button[2]")
+	private WebElement deleteButton;
+
+	public CulturalOfferDialog(WebDriver browser) {
+		super();
+		this.browser = browser;
+	}
+	
+	public void editButtonClick() {
+		this.editButton.click();
+	}
+	
+	public void deleteButtonClick() {
+		this.deleteButton.click();
+	}
+	
+	public void ensureEditButtonDisplayed() {
+		(new WebDriverWait(this.browser, Constants.TIMEOUT_WAIT)).until(ExpectedConditions.elementToBeClickable(this.editButton));		
+	}
+	
+	public void ensureDeleteButtonDisplayed() {
+		(new WebDriverWait(this.browser, Constants.TIMEOUT_WAIT)).until(ExpectedConditions.elementToBeClickable(this.deleteButton));		
+	}
+	
+}
