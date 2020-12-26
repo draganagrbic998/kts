785423889d1262e29a668ef0c733e90da4020590
Makim1998 <mmijatovic32@gmail.com>
2020-12-26 23:41:04 +0100

    e2e test registracija

--- e2e-testing/src/test/java/com/example/demo/TestConstants.java
+++ e2e-testing/src/test/java/com/example/demo/TestConstants.java
@@ -5,7 +5,8 @@ public class TestConstants {
 	public static final String CHROME_DRIVER_PATH = "chromedriver.exe";
 	public static final String FRONTEND_URL = "http://localhost:4200";
 	public static final String LOGIN_PATH = FRONTEND_URL + "/user/login";
-	public static final String PROFILE_PATH = FRONTEND_URL + "/user/profile";		
+	public static final String PROFILE_PATH = FRONTEND_URL + "/user/profile";
+	public static final String REGISTER_PATH = FRONTEND_URL + "/user/register";
 	public static final String HOME_PATH = FRONTEND_URL + "/";
 		
 	public static final String GUEST_EMAIL = "guest@gmail.com";
