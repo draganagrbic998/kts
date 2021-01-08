defa448d1a3aebd92c6b75f1ec7c150b7bb49ad2
unknown <nikolicpetar91@yahoo.com>
2021-01-08 04:27:48 +0100

    uradjeni testovi za image servis

--- frontend/src/app/shared/services/image.service.spec.ts
+++ frontend/src/app/shared/services/image.service.spec.ts
@@ -1,9 +1,11 @@
-import { TestBed } from '@angular/core/testing';
-
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { ImageService } from './image.service';
 
 describe('ImageService', () => {
   let service: ImageService;
+  const base64Mock = 'data:image/jpeg;base64,aGV5IHRoZXJl';
+  const byteArray: Uint8Array = new Uint8Array([104, 101, 121, 32, 116, 104, 101, 114, 101]);
+  const blobMock = new Blob([byteArray], {type: 'image/jpeg'});
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -13,4 +15,16 @@ describe('ImageService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should convert', done => {
+    let base64: string;
+    service.getBase64(blobMock).subscribe(
+      (path: string) => {
+        base64 = path;
+        expect(base64).toBe(base64Mock);
+        done();
+      }
+    );
+  });
+
 });
